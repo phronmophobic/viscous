a4b908558cb6253bcdb72734cd405b023add01de
Adrian Smith <smith.adriane@gmail.com>
2022-10-05 14:59:50 -0700

    Bumping to 1.3.1.

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]))
 
 (def lib 'com.phronemophobic/viscous)
-(def version "1.3")
+(def version "1.3.1")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
